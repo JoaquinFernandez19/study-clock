238d4cf7d595cfb9031471e0fc822048774de395
Joaquin Fernandez <copako12@gmail.com>
2020-04-14 11:17:46 -0300

    Updates

---
+++ precache-manifest.752f467d470a70d6f20c7cb26a2d1c50.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e3f0abb157b10ac607b847a0ca7fd7",
+    "url": "/study-clock/index.html"
+  },
+  {
+    "revision": "c13166559c288d6dbb90",
+    "url": "/study-clock/static/css/main.42884642.chunk.css"
+  },
+  {
+    "revision": "135293edcea490f09e78",
+    "url": "/study-clock/static/js/2.9829876c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/study-clock/static/js/2.9829876c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c13166559c288d6dbb90",
+    "url": "/study-clock/static/js/main.28c2040e.chunk.js"
+  },
+  {
+    "revision": "0b3269ea6ddddce65a6e",
+    "url": "/study-clock/static/js/runtime-main.d0df2750.js"
+  },
+  {
+    "revision": "ddf92754e4752b9a18937dbca33be711",
+    "url": "/study-clock/static/media/pop.ddf92754.mp3"
+  },
+  {
+    "revision": "a992299e163b45377d7afdd465c2419f",
+    "url": "/study-clock/static/media/pop2.a992299e.mp3"
+  },
+  {
+    "revision": "d751ccbc135c23e8fe74750f7eeb41ac",
+    "url": "/study-clock/static/media/reset.d751ccbc.mp3"
+  },
+  {
+    "revision": "887c9a31f6b7f8b8f4ae8a2b930fa9ef",
+    "url": "/study-clock/static/media/zen.887c9a31.mp3"
+  }
+]);
